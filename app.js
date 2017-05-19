2133b5f07a4f5ab3aba81f613e83605f3e57ae14
Cams03 <camille.francia@hotmail.fr>
2017-05-19 14:15:35 +0200

    premier test jquery getjson

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
-(function() {
-  var liste_courses = "http://courses.com/ajouter";
+let addItem =[]; //json pour ajouter item
+let Items = {
 
+    init: function (nom, qte) {
+        this.nom = nom,
+        this.qte = qte
+    },
+
+    decrire: function () {
+        let description = "Nom : " + this.nom + ", Qté : " + this.qte;
+        return description;
+      }
+  };
+
+document.getElementById('valider').addEventListener("click", function(){
+
+  nom = document.getElementById('nom').value;
+  qte = document.getElementById('qte').value;
+  let newItem = Object.create(Items);
+  newItem.init(nom, qte);
+  addItem.push(newItem);
+  console.log("Le nouvel item a été ajouté");
+    addItem.forEach(function (item) {
+        console.log(item.decrire());
+    });
+});
+console.log(addItem);
+
+(function() {
+  let liste_courses = "/";
+  let liste = [{nom: "", qt:"", id:""},{nom: "", qt:"", id:""}];
   $("#valider").on("click", function(){
 
     $.getJSON( liste_courses, {
       format: "json"
     })
-    .done(function( data ) {
-      $.each( data.items, function( i, item ) {
-        $( "li" ).appendTo( "#list" );
-        if ( i === 3 ) {
-          return false;
-        }
+    .done(function() {
+      $.each(item, function( nom, qte ) {
+        $( "<li>test</li>" ).appendTo( "#list" );
       });
     });
   })
+
+  $("form").submit(function(){
+    let $form = $( this );
+    let url = $form.attr( "action" );
+    let posting = $.post( url, addItem, json);
+
+    // Put the results in a div
+    posting.done(function() {
+      $.each(item, function( nom, qte ) {
+        $( "<li>test</li>" ).appendTo( "#list" );
+      });
+    });
+  });
+
 })();
